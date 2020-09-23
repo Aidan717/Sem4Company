01b416b3a9dad0d529e848d100af3909d32e1594
Kasper Jørgensen <1077990@ucn.dk>
2020-09-23 12:31:55 +0200

    test

--- Web_API_Service/Program.cs
+++ Web_API_Service/Program.cs
@@ -7,6 +7,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
+//this knew plox
 namespace Web_API_Service {
 	public class Program {
 		public static void Main(string[] args) {
