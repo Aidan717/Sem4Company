4d7f38c86613ba23e6d8fe100352f662371f876d
Kasper Jørgensen <1077990@ucn.dk>
2020-09-23 11:48:33 +0200

    fix ?

--- Program.cs
+++ Program.cs
@@ -7,6 +7,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
+
 namespace Web_API_Service {
 	public class Program {
 		public static void Main(string[] args) {
