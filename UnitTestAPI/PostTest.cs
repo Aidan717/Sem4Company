1625c9bc5672ac67f73437c948175c58e3549312
Mads Nymann Jensen <1074296@ucn.dk>
2020-10-18 19:10:31 +0200

    Added test methods to post and PostParametersNotMet

--- UnitTestAPI/PostTest.cs
+++ UnitTestAPI/PostTest.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Text.Json;
 using System.Threading.Tasks;
@@ -12,7 +13,6 @@ namespace UnitTestAPI {
     [TestClass]
     public class PostTest {
         Schools._Source testSchool;
-        ResponseStatus testResponse;
         TestApiController testApiCon;
 
         [TestInitialize]
@@ -35,12 +35,28 @@ namespace UnitTestAPI {
         public void TestPost() {
 
             //Arrange
-            testResponse = new ResponseStatus();
+            ResponseStatus testResponse = new ResponseStatus();
 
             //Act
+            var APIPostResult = testApiCon.Post("school", testSchool).Result.Value;
+            testResponse = APIPostResult;
 
             //Assert
+            Assert.IsTrue(testResponse.result.Equals("created"));
+        }
 
+        [TestMethod]
+        public void TestPostParametersNotMet() {
+
+            //Arrange
+            ResponseStatus testResponse = new ResponseStatus();
+
+            //Act
+            var APIPostResult = testApiCon.PostParametersNotMet("school", testSchool).Result.Value;
+            testResponse = APIPostResult;
+
+            //Assert
+            Assert.IsFalse(testResponse.result.Equals("created"));
         }
     }
 }
