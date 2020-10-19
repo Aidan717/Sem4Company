7b6ca99fc432dfe8fe9f1e105c3c0ba13cbfd94c
Christian Thordurson <1061674@ucn.dk>
2020-10-19 10:38:20 +0200

    Mail warning stuff (:

--- Web_API_Service/Controllers/MailController.cs
+++ Web_API_Service/Controllers/MailController.cs
@@ -17,30 +17,34 @@ namespace Web_API_Service.Controllers {
             this.mailService = mailService;
         }
 
-        [HttpPost("send")]
-        public async Task<IActionResult> SendMail([FromForm] MailRequest request) {
-            try {
-                await mailService.SendEmailAsync(request);
-                return Ok();
-            } catch (Exception ex) {
-                throw;
-            }
-        }
+        //[HttpPost("send")]
+        //public async Task<IActionResult> SendMail([FromForm] MailRequest request) {
+        //    try {
+        //        await mailService.SendEmailAsync(request);
+        //        return Ok();
+        //    } catch (Exception ex) {
+        //        throw;
+        //    }
+        //}
 
-        [HttpPost("welcome")]
-        public async Task<IActionResult> SendWelcomeMail([FromForm] WelcomeRequest request) {
-            try {
-                await mailService.SendWelcomeEmailAsync(request);
-                return Ok();
-            } catch (Exception ex) {
-                throw;
-            }
-        }
+        //[HttpPost("welcome")]
+        //public async Task<IActionResult> SendWelcomeMail([FromForm] WelcomeRequest request) {
+        //    try {
+        //        await mailService.SendWelcomeEmailAsync(request);
+        //        return Ok();
+        //    } catch (Exception ex) {
+        //        throw;
+        //    }
+        //}
 
         [HttpPost("warning")]
         public async Task<IActionResult> SendWarningMail([FromForm] MailRequest warningRequest) {
+            string Query = "Query ting og sager";
+            string Destination = "Destinations ting og sager";
+            string Error = "Error ting og sager";
+
             try {
-                await mailService.SendWarningEmailAsync(warningRequest);
+                await mailService.SendWarningEmailAsync(Query, Destination, Error);
                 return Ok();
             } catch (Exception ex) {
                 throw;
--- Web_API_Service/Models/MailSettings.cs
+++ Web_API_Service/Models/MailSettings.cs
@@ -5,7 +5,8 @@ using System.Threading.Tasks;
 
 namespace Web_API_Service.Models {
     public class MailSettings {
-        public string Mail { get; set; }
+        public string MailReciever { get; set; }
+        public string MailSender { get; set; }
         public string DisplayName { get; set; }
         public string Password { get; set; }
         public string Host { get; set; }
