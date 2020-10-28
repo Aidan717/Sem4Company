68355caf8779ca2c77f8f3753bfa4d217068dd5c
Mads Nymann Jensen <1074296@ucn.dk>
2020-10-28 13:09:48 +0100

    Making checkForError to retriving a DBschema

--- Web_API_Service/Controllers/ElkCheckController.cs
+++ Web_API_Service/Controllers/ElkCheckController.cs
@@ -1,8 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Text.Json;
 using System.Threading.Tasks;
+using MailKit;
 using Microsoft.AspNetCore.Mvc;
+using Web_API_Service.Models;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -20,9 +26,53 @@ namespace Web_API_Service.Controllers {
 
 		// GET api/<ValuesController>/5
 		//name need to change to what it does this is just temps
-		[HttpGet("{error}")]
-		public string checkForError() {
-			return "value";
+		[HttpGet("project/{error}")]
+		public async Task<ActionResult<DBSchema>> checkForError(string error) {
+			var result = new DBSchema();
+			HttpResponseMessage response = new HttpResponseMessage();
+
+			try {
+				using (var client = new HttpClient()) {
+
+					client.BaseAddress = new Uri("http://localhost:9200/" + "project" + "/_search");
+					client.DefaultRequestHeaders.Accept.Clear();
+					client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+					response = await client.GetAsync("?q=_exists_:" + error /*"activities.exceptions.errors.message"*/);
+
+					if (response.IsSuccessStatusCode) {
+
+						var option = new JsonSerializerOptions {
+							PropertyNameCaseInsensitive = true
+                        };
+
+						result = JsonSerializer.Deserialize<DBSchema>(await response.Content.ReadAsStringAsync(), option);
+						return result;
+					} else {
+						throw new HttpRequestException("statusCode: " + response.StatusCode);
+					}
+				}
+			} catch (HttpRequestException ex) {
+
+				//return result = new ResponseStatus("failed to connect" + ex.Message);
+				return result;
+			}
+
+			//Noter til mig selv:
+			//kigge hele databasen gennem for alle exception
+			//lav noget univercielt
+			//er der noget der hedder noget med exception
+			//indeholder den noget
+			//prøv at match exception med modellen
+			//from body er en match
+			//Tjekke hele listen i gennem
+			//Der er 3 der har en error returner dem
+			//Skal have en deafult check og en specifik
+			//Limit fejl til 24 timer eller de sidste 50 fejl
+			//Connect til kibana som vi gør i testAPI
+			//get alle error føst
+			//dernæst lav limit
+			//udvid langsomt
+			
 		}
 
 		//look up something specific with in the last hour that have given an exception or something
