f0fc29cdec52cbd44d778a9b6a3faea413716be4
Martin Thomsen <1078136@ucn.dk>
2020-11-01 17:04:01 +0100

    igang med Indexstat og GetIndex

--- Web_API_Service/Controllers/ElkCheckController.cs
+++ Web_API_Service/Controllers/ElkCheckController.cs
@@ -1,8 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using Web_API_Service.Models;
+using System.Text;
+using Web_API_Service.Service;
+using System.Text.Json;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -18,38 +25,51 @@ namespace Web_API_Service.Controllers {
 
 
 
-		// GET api/<ValuesController>/5
-		//name need to change to what it does this is just temps
-		[HttpGet("{error}")]
-		public string checkForError() {
-			return "value";
-		}
+        // GET api/<ValuesController>/5
+        //name need to change to what it does this is just temps
+        [HttpGet("{error}")]
+        //public async Task<ActionResult<DBSchema>> breh(string index) {
 
-		//look up something specific with in the last hour that have given an exception or something
-		[HttpGet("{error}")]
+        //    using (var client = new HttpClient()) {
+
+        //    }
+
+        //    return "value";
+        //}
+
+        //look up something specific with in the last hour that have given an exception or something
+        [HttpGet("{error}")]
 		public string LookUpSomeThingSpecific() {
 			return "value";
 		}
 
-		//look at status for index when a person call something
-		[HttpGet("{error}")]
-		public string LookforIndexStatus() {
-			return "value";
-		}
+        //look at status for index when a person call something
+        [HttpGet("db/{index}")]
+        public async Task<ActionResult<IndexStat>> GetIndexStatus() {
 
-		//look up history of a index and see if something does not fit in
-		[HttpGet("{error}")]
+            using (var client = new HttpClient()) {
+                var result = new IndexStat();
+                client.BaseAddress = new Uri("http://localhost:9200/");
+                client.DefaultRequestHeaders.Accept.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+               
+
+
+            }
+        }
+
+
+        //http://localhost:9200/_cat/indices?h=index laver en en liste af alle indexes
+        //http://localhost:9200/_cluster/health?level=indices henter alle indexes med en oversigt heriblandt status.
+
+
+
+
+        //look up history of a index and see if something does not fit in
+        [HttpGet("{error}")]
 		public string LookUphistory() {
 			return "value";
 		}
-
-
-
-
-
-
-
-
 
 
 		// POST api/<ValuesController>
---
+++ Web_API_Service/Models/IndexStat.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Web_API_Service.Models {
+    public class IndexStat {
+
+        public string id { get; set; }
+        public string status { get; set; }
+        public string index { get; set; }
+
+    }
+}
