9950680d0793fac97bf1deeed15daf9d5dde16be
Martin Thomsen <1078136@ucn.dk>
2020-11-04 12:16:40 +0100

    GetIndexStatus

--- Web_API_Service/Controllers/ElkCheckController.cs
+++ Web_API_Service/Controllers/ElkCheckController.cs
@@ -172,9 +172,9 @@ namespace Web_API_Service.Controllers {
 		}
 
 		//look at status for index when a person call something
-		[HttpGet("hey")]
-		public async Task<ActionResult<IndexStat>> GetIndexStatus(string index) {
-			var result = new IndexStat();
+		[HttpGet("hey/{index}")]
+		public async Task<ActionResult<object>> GetIndexStatus(string index) {
+			var result = new clusterHealth("Fail report");
 			HttpResponseMessage response = new HttpResponseMessage();
 
 			try {
@@ -184,20 +184,37 @@ namespace Web_API_Service.Controllers {
 					client.DefaultRequestHeaders.Accept.Clear();
 					client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
 
-					response = await client.GetAsync("schools");
+					if (index.Equals("as")) {
+						response = await client.GetAsync("/_cluster/health/schools");
 
+					} else {
+						response = await client.GetAsync("schools/_stats");
+
+					}
 					if (response.IsSuccessStatusCode) {
 
-						result = JsonSerializer.Deserialize<IndexStat>(await response.Content.ReadAsStringAsync());
+						result = JsonSerializer.Deserialize<clusterHealth>(await response.Content.ReadAsStringAsync());
+						if (index.Equals("as")) {
+							result = JsonSerializer.Deserialize<clusterHealth>(await response.Content.ReadAsStringAsync());
+
+						} else {
+							var results = new IndexStats();
+							results = JsonSerializer.Deserialize<IndexStats>(await response.Content.ReadAsStringAsync());
+							return result;
+						}
+						if (result.status == ("red"))
+							return result.status;
+
 						return result;
+
 					} else {
 						throw new HttpRequestException("statusCode: " + response.StatusCode);
 					}
 				}
-			}catch(HttpRequestException ex) {
+			} catch (HttpRequestException ex) {
+				return result;
 
-			return result;
-            }
+			}
 
 		}
 
