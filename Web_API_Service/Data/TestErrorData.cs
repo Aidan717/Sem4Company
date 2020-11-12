5b0a394b0e2d46f00284a1aac601ac21d9568910
Christian Thordurson <1061674@ucn.dk>
2020-11-12 12:39:00 +0100

    test clustering error

---
+++ Web_API_Service/Data/TestErrorData.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Web_API_Service.Data {
+    static class TestErrorData {
+
+        internal static readonly ErrorData Error = new ErrorData {
+            Error = 0
+        };
+    }
+}
--- Web_API_Service/Program.cs
+++ Web_API_Service/Program.cs
@@ -1,16 +1,23 @@
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.ML;
+using Web_API_Service.Data;
 
 //some knew
 namespace Web_API_Service {
 	public class Program {
-		public static void Main(string[] args) {
+
+        static readonly string _dataPath = Path.Combine(Environment.CurrentDirectory, "Data", "iris.data");
+        static readonly string _modelPath = Path.Combine(Environment.CurrentDirectory, "Data", "IrisClusteringModel.zip");
+
+        public static void Main(string[] args) {
 			CreateHostBuilder(args).Build().Run();
 		}
 
@@ -20,4 +27,27 @@ namespace Web_API_Service {
 					webBuilder.UseStartup<Startup>();
 				});
 	}
+
+//    public void ErrorClustering() {
+//        var mlContext = new MLContext(seed: 0);
+
+//        IDataView dataView = mlContext.Data.LoadFromTextFile<ErrorData>(_dataPath, hasHeader: false, separatorChar: ',');
+
+//        string featuresColumnName = "Features";
+//        var pipeline = mlContext.Transforms
+//            .Concatenate(featuresColumnName, "SepalLength", "SepalWidth", "PetalLength", "PetalWidth")
+//            .Append(mlContext.Clustering.Trainers.KMeans(featuresColumnName, numberOfClusters: 3));
+
+//        var model = pipeline.Fit(dataView);
+
+//        using (var fileStream = new FileStream(_modelPath, FileMode.Create, FileAccess.Write, FileShare.Write)) {
+//            mlContext.Model.Save(model, dataView.Schema, fileStream);
+
+//            var predictor = mlContext.Model.CreatePredictionEngine<ErrorData, ClusterPrediction>(model);
+
+//            var prediction = predictor.Predict(TestErrorData.Error);
+//            Console.WriteLine($"Cluster: {prediction.PredictedError}");
+//            Console.WriteLine($"Distances: {string.Join(" ", prediction.Distances)}");
+//        }
+//    }
 }
